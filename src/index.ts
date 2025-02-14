5fd1e69299c0ba051416614ece3d1465672de456
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-14 09:55:59 -0600

    implementation of sorter in character class and linkedlist

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,18 @@ import { Sorter } from "./Sorter";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-numbersCollection.sort();
-
-console.log(numbersCollection.data);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// numbersCollection.sort();
+// console.log(numbersCollection.data);
 
 // const charactersCollection = new CharactersCollection("Xaaybasw");
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
+// charactersCollection.sort();
 // console.log(charactersCollection.data);
 
-// const linkedList = new LinkedList();
-// linkedList.add(500);
-// linkedList.add(-10);
-// linkedList.add(-3);
-// linkedList.add(4);
-
-// const sorter = new Sorter(linkedList);
-// sorter.sort();
-// linkedList.print();
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
